ac13e3f9003ca91a1cdca3ef55749dc51ffb3358
bcooo <102440125+bcooo@users.noreply.github.com>
2022-03-27 22:41:24 +0800

    Add files via upload

--- 0xray.sh
+++ 0xray.sh
@@ -259,6 +259,12 @@ getData() {
         echo " "
         
         
+        answer="y"
+        
+        
+        if [[ "${answer,,}" != "y" ]]; then
+            exit 0
+        fi
 
         echo ""
         while true
@@ -288,11 +294,7 @@ getData() {
     echo ""
     if [[ "$(needNginx)" = "no" ]]; then
         if [[ "$TLS" = "true" ]]; then
-        
-            
-           ${PORT}='443'
-            
-            
+            read -p " 请输入xray监听端口[强烈建议443，默认443]：" PORT
             [[ -z "${PORT}" ]] && PORT=443
         else
             read -p " 请输入xray监听端口[100-65535的一个数字]：" PORT
@@ -360,9 +362,7 @@ getData() {
         colorEcho $BLUE " 请选择流控模式:" 
         echo -e "   1) xtls-rprx-direct [$RED推荐$PLAIN]"
         echo "   2) xtls-rprx-origin"
-        
-        $answer='1'
-        
+        read -p "  请选择流控模式[默认:direct]" answer
         [[ -z "$answer" ]] && answer=1
         case $answer in
             1)
@@ -408,10 +408,7 @@ getData() {
         echo "   3) 美女站(https://imeizi.me)"
         echo "   4) 高清壁纸站(https://bing.imeizi.me)"
         echo "   5) 自定义反代站点(需以http或者https开头)"
-        
-        
-        $answer=5
-        
+        read -p "  请选择伪装网站类型[默认:高清壁纸站]" answer
         if [[ -z "$answer" ]]; then
             PROXY_URL="https://bing.imeizi.me"
         else
@@ -443,10 +440,6 @@ getData() {
                 ;;
             5)
                 read -p " 请输入反代站点(以http或者https开头)：" PROXY_URL
-                
-$PROXY_URL='https://m.zongheng.com'
-                
-                
                 if [[ -z "$PROXY_URL" ]]; then
                     colorEcho $RED " 请输入反代网站！"
                     exit 1
@@ -467,11 +460,7 @@ $PROXY_URL='https://m.zongheng.com'
         colorEcho $BLUE "  是否允许搜索引擎爬取网站？[默认：不允许]"
         echo "    y)允许，会有更多ip请求网站，但会消耗一些流量，vps流量充足情况下推荐使用"
         echo "    n)不允许，爬虫不会访问网站，访问ip比较单一，但能节省vps流量"
-    
-        
-        $answer='n'
-        
-        
+        read -p "  请选择：[y/n]" answer
         if [[ -z "$answer" ]]; then
             ALLOW_SPIDER="n"
         elif [[ "${answer,,}" = "y" ]]; then
@@ -483,9 +472,7 @@ $PROXY_URL='https://m.zongheng.com'
     fi
 
     echo ""
-    
-    $NEED_BBR" ='Y'
-    
+    read -p " 是否安装BBR(默认安装)?[y/n]:" NEED_BBR
     [[ -z "$NEED_BBR" ]] && NEED_BBR=y
     [[ "$NEED_BBR" = "Y" ]] && NEED_BBR=y
     colorEcho $BLUE " 安装BBR：$NEED_BBR"
